40a1a4fac5cc405689d345cea7a3750e717745de
kaya <jepsonkaya@gmail.com>
2019-04-26 09:45:16 -0700

    add test

--- WordCounter.Tests/Models.Tests/Words.Tests.cs
+++ WordCounter.Tests/Models.Tests/Words.Tests.cs
@@ -9,10 +9,36 @@ namespace WordCounter.Tests
     [TestMethod]
     public void IsWord_IsAWord_True()
     {
-      WordCounter newWord = new WordCounter();
+      WordCountGen newWord = new WordCountGen();
       Assert.AreEqual(true, newWord.IsWord("test"));
     }
+    [TestMethod]
+    public void IsSentence_IsASentence_True()
+    {
+      WordCountGen newSentence = new WordCountGen();
+      Assert.AreEqual(true, newSentence.IsSentence("the cat in the hat"));
+    }
 
+    [TestMethod]
+    public void SentenceToArray_UserSentenceToArray_True()
+    {
+      WordCountGen newArray = new WordCountGen();
+      Assert.AreEqual(true, newArray.SentenceToArray("the cat in the hat"));
+    }
+    //
+    // [TestMethod]
+    // public void WorthOne_LetterAEqualsOne_True()
+    // {
+    //   ScrabbleScore newWord = new ScrabbleScore();
+    //   Assert.AreEqual(1,  newWord.LookUpValue('a'));
+    // }
+    //
+    // [TestMethod]
+    // public void WorthFive_WordEqualsFive_True()
+    // {
+    //   ScrabbleScore newWord = new ScrabbleScore();
+    //   Assert.AreEqual(5,  newWord.LoopThroughWord("dad"));
+    // }
 
   }
 }
